35cd7b892a1bc52d13966841c5ffe8332a7ff4f6
Claudio Barsante <clbmribas@gmail.com>
2021-12-10 11:58:46 -0300

    feat: add create user page

---
+++ src/pages/users/create.tsx
@@ -0,0 +1,59 @@
+import {
+  Box,
+  Flex,
+  Heading,
+  Button,
+  Icon,
+  Table,
+  Thead,
+  Tr,
+  Th,
+  Checkbox,
+  Tbody,
+  Td,
+  Text,
+  Divider,
+  VStack,
+  SimpleGrid,
+  HStack
+} from '@chakra-ui/react';
+import Header from './../components/Header';
+import Sidebar from './../components/Sidebar';
+import Input from './../components/Form/Input';
+
+export default function CreateUser() {
+  return (
+    <Box>
+      <Header />
+      <Flex w="100%" my="6" maxWidth={1480} mx="auto" px="6">
+        <Sidebar />
+        <Box flex="1" borderRadius={8} bg="gray.800" p="8">
+          <Heading size="lg" fontWeight="normal">
+            Create user
+          </Heading>
+          <Divider my="6" borderColor="gray.700" />
+          <VStack spacing="8">
+            <SimpleGrid minChildWidth="240px" spacing="8" w="100%">
+              <Input name="name" label="Name" type="text" />
+              <Input name="email" label="E-mail" type="email" />
+            </SimpleGrid>
+            <SimpleGrid minChildWidth="240px" spacing="8" w="100%">
+              <Input name="password" label="Password" type="password" />
+              <Input
+                name="password-confirmation"
+                label="Confirm password"
+                type="password"
+              />
+            </SimpleGrid>
+          </VStack>
+          <Flex mt="8" justify="flex-end">
+            <HStack spacing="4">
+              <Button colorScheme="whiteAlpha">Cancel</Button>
+              <Button colorScheme="pink">Add</Button>
+            </HStack>
+          </Flex>
+        </Box>
+      </Flex>
+    </Box>
+  );
+}
